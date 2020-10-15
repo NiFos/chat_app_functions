9a2a3fcbdbc188cce42eedc287d4d0d46d07aae4
Vlad <vlad.nifos@gmail.com>
2020-10-15 17:55:04 +0300

    Dev: cors and cookie

--- index.ts
+++ index.ts
@@ -3,14 +3,21 @@ import express from "express";
 import { auth } from "./functions/auth";
 import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 const app = express();
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
 app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(
+  cors({
+    credentials: true,
+    origin: ["http://localhost:8080", "http://localhost:3000"],
+  })
+);
+app.use(bodyParser.json());
 app.use("/auth", auth);
 
-app.listen(3000, () => {
+app.listen(3001, () => {
   console.log("Dev server started!");
 });
